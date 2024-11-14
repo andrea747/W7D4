b9398ec9b4f1f8c4868b9a9027425b9ff230b275
andrea747 <andreapadulazzi@gmail.com>
2024-11-14 16:42:05 +0100

    .

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("load-images-btn");
+
+const apiKey = "[70SEI0AnXRs60iNnHPdeM1032dpRKwAwuYMZz5zk6Ou9DOqb8zUl07c5]";
+const url = "https://api.pexels.com/v1/search?query=winter";
